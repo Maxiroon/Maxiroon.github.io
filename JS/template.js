2b6fc027b9509fff8c0fb009a4378b56667668dc
Maxiroon <contactmaxiroon@gmail.com>
2025-05-27 12:09:39 -0500

    Nav changes and 4 year page

---
+++ JS/template.js
@@ -0,0 +1,8 @@
+document.getElementById("header").innerHTML = `
+    <div id="topnav">
+        <a href="/">Home</a>
+        <a href="/releases">Music</a>
+        <a href="/about">About Maxiroon</a>
+        <a href="https://cpi.maxiroon.xyz/">Community Picked Islands</a>
+        <a class="purple" href="/4year">4TH channel Anniversary</a>
+    </div>`;
