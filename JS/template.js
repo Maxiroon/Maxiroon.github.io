dac015563bce159e4cdc29938a1bbaf9f1e95461
Maxiroon <contactmaxiroon@gmail.com>
2025-06-18 01:20:39 -0500

    Hypernatural page + footer
    - New Hypernatural pages
    - Changed footer to reference template.js

--- JS/template.js
+++ JS/template.js
@@ -2,7 +2,16 @@ document.getElementById("header").innerHTML = `
     <div id="topnav">
         <a href="/">Home</a>
         <a href="/releases">Music</a>
-        <a href="/about">About Maxiroon</a>
+        <a href="/about">About</a>
         <a href="https://cpi.maxiroon.xyz/">Community Picked Islands</a>
-        <a class="purple" href="/4year">4TH channel Anniversary</a>
+        <a class="ethereal" href="/releases/msm/hypernatural">Hypernaturals</a>
     </div>`;
+
+document.getElementById("footer").innerHTML = `
+    <br>
+    <center>
+        <img src="/media/textred.png" alt="MAXIROON" width="150px">
+        <br>
+        <a href="/">Home Page</a>
+    </center>
+    <br>`;
