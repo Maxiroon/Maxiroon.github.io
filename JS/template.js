e087516e6958eebead745f93de62c98dccd3e4bd
Maxiroon <contactmaxiroon@gmail.com>
2025-07-09 13:19:10 -0500

    Nav and header changes

--- JS/template.js
+++ JS/template.js
@@ -1,16 +1,26 @@
-document.getElementById("header").innerHTML = `
-    <div id="topnav">
-        <a href="/">Home</a>
-        <a href="/music">Music</a>
-        <a href="/blog">Blog</a>
-        <a class="cpi" href="https://cpi.maxiroon.xyz/">Community Picked Islands</a>
-        <a class="ethereal" href="https://hypernatural.maxiroon.xyz">Hypernaturals</a>
-    </div>`;
+document.getElementById("nav").innerHTML = `
+    <nav>
+        <div id="topnav">
+            <a href="/">Home</a>
+            <a href="/music">Music</a>
+            <a href="/blog">Blog</a>
+            <a class="cpi" href="https://cpi.maxiroon.xyz/">Community Picked Islands</a>
+            <a class="ethereal" href="https://hypernatural.maxiroon.xyz">Hypernaturals</a>
+        </div>
+        <br><br><br>
+    <nav>
+    `;
+
+document.getElementById("heading").innerHTML = `
+    <head>
+        <center><img src="/media/textgradient.png" alt="MAXIROON" style="border-width: 0px; border-radius: 0px;">
+        <hr><br><br>
+    </head>`;
 
 document.getElementById("footer").innerHTML = `
     <br>
     <center>
-        <img src="/media/textred.png" alt="MAXIROON" width="150px">
+        <img src="/media/textred.png" alt="MAXIROON" width="150px" style="border-width: 0px; border-radius: 0px;">
         <br>
         <a href="/" style="color: white;">Home Page</a><a>ㅤ</a><a href="https://cpi.maxiroon.xyz" style="color: white;">CPI subdomain</a><a>ㅤ</a><a href="https://hypernatural.maxiroon.xyz" style="color: white;">Hypernatural subdomain</a>
     </center>
