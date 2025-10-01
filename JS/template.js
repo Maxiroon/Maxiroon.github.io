f7fddd2c4662e9f7458465d24f66e27d80f5ff29
Maxiroon <contactmaxiroon@gmail.com>
2025-09-30 22:40:32 -0500

    Branding change
    Branding colors changed from red to blue

--- JS/template.js
+++ JS/template.js
@@ -17,7 +17,7 @@ document.getElementById("nav").innerHTML = `
 document.getElementById("footer").innerHTML = `
     <br>
     <center>
-        <img src="/media/textred.png" alt="MAXIROON" width="150px" style="border-width: 0px; border-radius: 0px;">
+        <img src="/media/textwhite.webp" alt="MAXIROON" width="150px" style="border-width: 0px; border-radius: 0px;">
         <br>
         <a href="/" style="color: white;">Home Page</a><a>ㅤ</a><a href="https://cpi.maxiroon.xyz" style="color: white;">CPI subdomain</a><a>ㅤ</a><a href="https://hypernatural.maxiroon.xyz" style="color: white;">Hypernatural subdomain</a>
     </center>
@@ -27,6 +27,6 @@ document.getElementById("heading").innerHTML = `
     <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9640811349836436"
      crossorigin="anonymous"></script>
-        <center><div class="head"><img src="/media/textgradient.png" alt="MAXIROON" style="border-width: 0px; border-radius: 0px;"></div>
+        <center><div class="head"><img src="/media/textwhite.webp" alt="MAXIROON" style="border-width: 0px; border-radius: 0px;"></div>
         <hr><br><br>
     </head>`;
